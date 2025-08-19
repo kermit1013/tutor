7eee00d293d9d391f4b1f936ffcf49102cc4ba0c
Kermit <k84101311@gmail.com>
2025-08-19 19:04:08 +0800

    added comments

--- src/main/java/com/gtalent/tutor/configs/JwtAuthFilter.java
+++ src/main/java/com/gtalent/tutor/configs/JwtAuthFilter.java
@@ -21,20 +21,31 @@ import java.util.Optional;
 
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
-    @Autowired
-    private JwtService jwtService;
+    //驗證流程的核心：基於 OncePerRequestFilter 自定義的一個過濾器 JwtAuthFilter
+    //確保每個請求都會經過此過濾器一次
 
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+
+    //依賴注入
     @Autowired
-    private UserRepository userRepository;
+    public JwtAuthFilter(JwtService jwtService, UserRepository userRepository) {
+        this.jwtService = jwtService;
+        this.userRepository = userRepository;
+    }
 
     @Override
+    //必須實作繼承類的抽象方法 == 過濾器執行的主要邏輯...
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // 從http headers中獲取Authorization欄位-> Bearer ......
         String authHeader = request.getHeader("Authorization");
-        // 1. 檢查Authorization格式是否正確
+        // 檢查Authorization格式是否正確
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            //該次請求過濾器結束生命週期=>將請求繼續往下傳遞...
             filterChain.doFilter(request, response);
             return;
         }
+        // 若開頭格式（Bearer...）正確，則擷取第七字元開始的字串（實際jwť）
         String jwtToken = authHeader.substring(7);
         String username = jwtService.getUsernameFromToken(jwtToken);
         if(username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
@@ -42,14 +53,15 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             Optional<User> user = userRepository.findByUsername(username);
             //todo 驗證token是否過期或無效
             if (user.isPresent()) {
-
                 //*** 若使用Spring Security (library)必須包含 授權 (Authorization)邏輯->「該用戶能做什麼？」***
                 List<? extends GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("ROLE_USER"));
+                //該token並非jwt token，而是Spring Security內部使用的token(包含user & authorities)
                 UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(user.get(), null, authorities);
+                // 將 內部使用的token 投進 Spring Security 認證箱（SecurityContextHolder）
                 SecurityContextHolder.getContext().setAuthentication(authenticationToken);
             }
         }
-
+        //該次請求過濾器結束生命週期=>將請求繼續往下傳遞...
         filterChain.doFilter(request, response);
     }
 }
--- src/main/java/com/gtalent/tutor/configs/SecurityConfig.java
+++ src/main/java/com/gtalent/tutor/configs/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.gtalent.tutor.configs;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -10,18 +11,27 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+//向spring boot標示該類別為設定檔，Spring將會在啟動時讀取
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private final JwtAuthFilter jwtAuthFilter;
+
     @Autowired
-    private JwtAuthFilter jwtAuthFilter;
+    public SecurityConfig(JwtAuthFilter jwtAuthFilter) {
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/products/**").authenticated()
-                        .anyRequest().permitAll()
+                        .requestMatchers("/jwt/**").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/products/**").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/v2/users/**").permitAll()
+                        // /products/** 包含 /products
+                        .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
